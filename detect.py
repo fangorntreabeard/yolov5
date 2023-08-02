d5a5a2e63c04e5b3a64943906c4bc97b9cd9cc1e
Fangorn <126914629+fangorntreabeard@users.noreply.github.com>
2023-08-02 20:21:07 +0700

    Update detect.py
    Signed-off-by: Fangorn <126914629+fangorntreabeard@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -166,7 +166,7 @@ def run(
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(f'{txt_path}.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                        with open(f'/confidence.txt', 'a+') as f:
+                        with open(f'yolov5/confidence.txt', 'a+') as f:
                             f.write(f'{Path(txt_path).name}\t{cls}\t{conf}\n')
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
